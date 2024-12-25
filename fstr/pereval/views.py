5583b63e5f698229f8f28854d98db34e03d6512e
VVither <kashirovb@gmail.com>
2024-12-25 06:15:15 +0400

    add view

--- fstr/pereval/views.py
+++ fstr/pereval/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import PerevalSerializer
 
-# Create your views here.
+class SubmitDataView(APIView):
+    def post(self, request):
+        serializer = PerevalSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": status.HTTP_201_CREATED, "mesage": "Данные успешно сохранены"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"status": status.HTTP_400_BAD_REQUEST, "mesage": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
