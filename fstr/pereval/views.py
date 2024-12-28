bfdd93bd89b8aa7314191c3e7f9828b448a767bd
VVither <kashirovb@gmail.com>
2024-12-28 17:25:17 +0400

    add method GET

--- fstr/pereval/views.py
+++ fstr/pereval/views.py
@@ -1,7 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
 from .serializers import PerevalSerializer
+from .models import pereval_added
 
 class SubmitDataView(APIView):
     def post(self, request):
@@ -15,4 +18,16 @@ class SubmitDataView(APIView):
     def get(self, request, pk=None):
         if pk:
             try:
-                
+                pereval = get_object_or_404(pereval_added, pk=pk)
+                serializer = PerevalSerializer(pereval)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except Exception as e:
+                return Response({'state': 0, 'message': f'Перевал с ID {pk} не найден'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            email = request.query_params.get('user__email=email')    
+            if email:
+                perevals = pereval_added.objects.filter(user__email=email)
+                serializer = PerevalSerializer(perevals, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response({'state': 0, 'message': 'Не передан Email'}, status=status.HTTP_400_BAD_REQUEST)
